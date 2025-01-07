4fb047b1b5195fa0c7871c35a42f522edc29654f
luv1024.read@gmail.com <luv1024.read@gmail.com>
2025-01-08 01:53:59 +0900

    コメント追加。

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,5 +1,6 @@
 /**
  * 記事を最新順にソートする関数
+ * Astro.globで取得したデータ以外には使わない。
  * @function
  * @param {*} target Astro.glob(*.md)で取得した記事
  * @returns 
@@ -11,6 +12,7 @@ export const itemSort = (target) => target.sort((a, b) => {
 });
 /**
  * 下書き(draft)以外を抽出する関数
+ * Astro.globで取得したデータ以外には使わない。
  * @param {*} target Astro.glob(*.md)で取得した記事
  * @param {*} draft 下書きがtrueの記事(hoge.frontmatter.draft)
  * @returns 
