f382c8574ff008228c07b9357fb00f0ceaf1684f
luv1024.read@gmail.com <luv1024.read@gmail.com>
2025-01-19 00:44:14 +0900

    記事の投稿日時を年月日にフォーマットするモジュールを作成。

--- src/utils/util.js
+++ src/utils/util.js
@@ -12,9 +12,22 @@ export const itemSort = (target) => target.sort((a, b) => {
 });
 /**
  * 下書き(draft)以外を抽出する関数
- * Astro.globで取得したデータ以外には使わない。
+ * 注 Astro.globで取得したデータ以外には使わない。
  * @param {*} target Astro.glob(*.md)で取得した記事
  * @param {*} draft 下書きがtrueの記事(hoge.frontmatter.draft)
  * @returns 
  */
 export const releasedSort = (target) => target.filter((post) => !post.frontmatter.draft);
+
+/**
+ * 記事の投稿日時を年月日形式で取得する関数
+ * @param {*} postDate frontmatter.pubDate
+ * @returns 
+ */
+export const dispPubDate = (postDate) => {
+    const getPubDate = new Date(postDate);
+    const getPostYear = getPubDate.getFullYear();
+    const getPostMonth = String(getPubDate.getMonth() + 1).padStart(2, '0');  // 月は0から始まるため+1
+    const getPostDay = String(getPubDate.getDate()).padStart(2, '0');
+    return `${getPostYear}年${getPostMonth}月${getPostDay}日`;
+}
