b8b38c223115f5233ba328bee77bdad2dde9b745
luv1024.read@gmail.com <luv1024.read@gmail.com>
2025-01-22 00:58:07 +0900

    github pageにデプロイするための設定追加。

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://S-Shima-OKA.github.io/',
+  outDir: "./docs",
+  base: '/nerd-out',
   output: 'static',
   publicDir: 'public',
   integrations: [tailwind()]
