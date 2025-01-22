ff4d378e1c8864cedad046cd80f739ca366a87bd
luv1024.read@gmail.com <luv1024.read@gmail.com>
2025-01-22 23:22:43 +0900

    siteのパス修正。

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nerd-out.github.io',
+  site: 'https://github.com/S-Shima-OKA/nerd-out',
   // outDir: "./docs",
-  base: '/nerd-out', //ローカル確認時はコメントアウト
+  // base: '/nerd-out', //ローカル確認時はコメントアウト
   output: 'static',
   publicDir: 'public',
   integrations: [tailwind()]
