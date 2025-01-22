90b8bf7c6c70043231af7c0fc7a556ac503c0cf8
luv1024.read@gmail.com <luv1024.read@gmail.com>
2025-01-23 00:06:18 +0900

    siteURLを修正。

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://s-shima-oka.github.io',
+  site: 'https://s-shima-oka.github.io/',
   // base: '/nerd-out', //ローカル確認時はコメントアウト
   // output: 'static',
   // publicDir: 'public',
