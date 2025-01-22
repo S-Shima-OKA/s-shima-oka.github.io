c23251bcc7fb17f304d45160c5d37ded28516c3b
luv1024.read@gmail.com <luv1024.read@gmail.com>
2025-01-23 00:00:28 +0900

    baseを修正。

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://s-shima-oka.github.io',
-  // base: 'nerd-out', //ローカル確認時はコメントアウト
+  base: '/nerd-out', //ローカル確認時はコメントアウト
   // output: 'static',
   // publicDir: 'public',
   integrations: [tailwind()]
